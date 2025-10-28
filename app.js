3a30d41ece03c9f982e45dc6fcbd7608a0b203bb
Francis <brakay1136@gmail.com>
2025-10-28 10:52:45 -0700

    win and draw  functionality done

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ class GameLogic {
 
     this.currentPlayer = "X";
     this.clickCell = [];
+    this.winningpatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    this.countMoves = 0;
   }
 
   checkSelectedCell(e) {
@@ -22,7 +34,7 @@ class GameLogic {
         }
       }
     }
-    this.checkWinner();
+    this.countMoves++, this.checkWinner();
   }
 
   checkWinner() {
@@ -31,21 +43,33 @@ class GameLogic {
     allCell.forEach((cell) => {
       this.clickCell.push(cell.textContent);
     });
-    console.log(this.clickCell);
+    for (let pattern of this.winningpatterns) {
+      let index1 = pattern[0];
+      let index2 = pattern[1];
+      let index3 = pattern[2];
+
+      let value1 = this.clickCell[index1];
+      let value2 = this.clickCell[index2];
+      let value3 = this.clickCell[index3];
+
+      if (value1 === value2 && value2 === value3 && value1 !== "") {
+        document.querySelector(
+          ".winnerBtnDisplay"
+        ).textContent = `${value1} won!`;
+        return;
+      }
+    }
+    if (this.countMoves === this.clickCell.length) {
+      document.querySelector(".winnerBtnDisplay").textContent = "Its a draw!";
+    }
   }
 
   resetBoard() {
     document.querySelectorAll(".cell").forEach((btn) => (btn.textContent = ""));
-    this.currentPlayer = "X";
+    this.currentPlayer = "O";
     this.turn.textContent = this.currentPlayer;
+    document.querySelector(".winnerBtnDisplay").textContent = "";
   }
-
-  //Tomorrows checklist
-  /*
-    1. Track or create the win and draw functionality
-    2. Display who ever won in it card
-  
-  */
 
   addEventListeners() {
     this.board.addEventListener("click", (e) => this.checkSelectedCell(e));
